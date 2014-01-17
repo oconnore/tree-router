8b68565559c3df061cda737d19c4bc6c53ed928c
Eric O'Connor <eric@bocoup.com>
2014-01-16 22:23:00 -0500

    Publish to NPM

---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  Collections: require(__dirname + '/collections.js'),
+  Server: require(__dirname + '/server.js'),
+  PathNode: require(__dirname + '/pathnode.js'),
+  Response: require(__dirname + '/response.js'),
+  Utils: require(__dirname + '/utils.js'),
+};
